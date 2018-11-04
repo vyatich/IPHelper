14f8c8dae20caf229dd38eab14da910cf669d12a
viktor <vik_ivy@mail.ru>
2018-11-04 16:13:45 +0600

    Refactoring main method. Create methods for conversation long to ip address (longToIp) and ip address to long (ipToLong).

--- src/main/java/com/helper/Application.java
+++ src/main/java/com/helper/Application.java
@@ -4,16 +4,12 @@ package com.helper;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.Inet4Address;
 
 public class Application {
     public static void main(String[] args) {
-
         //todo will add logging;
-        Inet4Address startAdress = null;
-        Inet4Address endAdress = null;
-        String startIP;
-        String endIP;
+        String startIP = "";
+        String endIP = "";
         System.out.println("Please enter first IP in range");
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             startIP = reader.readLine();
@@ -22,6 +18,33 @@ public class Application {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        long startIp = ipToLong(startIP);
+        long endIp = ipToLong(endIP);
+        for (long adress = startIp + 1; adress < endIp; adress++) {
+            System.out.println(longToIp(adress));
+        }
 
+
+    }
+    static long ipToLong(String ipAddress) {
+        long result = 0;
+        String[] ipAddressInArray = ipAddress.split("\\.");
+        for (int i = 3; i >= 0; i--) {
+            long ip = Long.parseLong(ipAddressInArray[3 - i]);
+            // left shifting 24,16,8,0 and bitwise OR
+            // 1. 192 << 24
+            // 1. 168 << 16
+            // 1. 1 << 8
+            // 1. 2 << 0
+            result |= ip << (i * 8);
+        }
+        return result;
+    }
+
+    static String longToIp(long i) {
+        return ((i >> 24) & 0xFF) +
+                "." + ((i >> 16) & 0xFF) +
+                "." + ((i >> 8) & 0xFF) +
+                "." + (i & 0xFF);
     }
 }
